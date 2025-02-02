8446004fe11dc3351a106f7df056230c660197cd
github.com-lj <ligan.june@gmail.com>
2025-02-02 22:16:14 +0800

    Fix the browser refresh issue where when you refresh from non-initial uri it will always go back to the fixed initial uri

--- lib/routes/route_config.dart
+++ lib/routes/route_config.dart
@@ -7,25 +7,34 @@ import 'package:my_portfolio_flutter/routes/route_names.dart';
 
 class RouteConfig {
   static GoRouter returnRouter() {
-    return GoRouter(initialLocation: "/", routes: [
-      GoRoute(
-          path: "/", // my portfolio
-          name: RouteNames.initial,
-          pageBuilder: (context, state) {
-            return const MaterialPage(child: HomePage());
-          }),
-      GoRoute(
-          path: "/login",
-          name: RouteNames.login,
-          pageBuilder: (context, state) {
-            return const MaterialPage(child: LoginPage());
-          }),
-      GoRoute(
-          path: "/dashboard",
-          name: RouteNames.dashboard,
-          pageBuilder: (context, state) {
-            return const MaterialPage(child: DashboardPage());
-          }),
-    ]);
+    return GoRouter(
+        initialLocation: Uri.base.toString().replaceFirst(
+              '${Uri.base.origin}/#', // this is to stay on the same location
+              '', // when refreshes or page reload
+            ), // problem: i.e. http://localhost:52472/#/login when you reload
+        // it will go back to the initiallocation if the value is fixed, so let's
+        // say, the fixed value is '/', so when you reload/refresh the browser
+        // it will go back to http://localhost:52472 instead of staying to
+        // #/login
+        routes: [
+          GoRoute(
+              path: "/", // my portfolio
+              name: RouteNames.portfolio,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: HomePage());
+              }),
+          GoRoute(
+              path: "/login",
+              name: RouteNames.login,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: LoginPage());
+              }),
+          GoRoute(
+              path: "/dashboard",
+              name: RouteNames.dashboard,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: DashboardPage());
+              }),
+        ]);
   }
 }
--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,6 +1,7 @@
 class RouteNames {
 
   static const String initial = "initial";
+  static const String portfolio = "portfolio";
   static const String login = "login";
   static const String dashboard = "dashboard";
 }
