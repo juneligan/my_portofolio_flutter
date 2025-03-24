b7a420f6b2638d9cd15cf6e2ca59cf499438033e
github.com-lj <ligan.june@gmail.com>
2025-03-24 08:21:40 +0800

    Update to use mobile server

--- lib/linklytics/be_integration/api_route_names.dart
+++ lib/linklytics/be_integration/api_route_names.dart
@@ -1,6 +1,6 @@
 class ApiRouteNames {
-  // static const String baseUrl = 'https://amusing-open-javelin.ngrok-free.app/';
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'https://amusing-open-javelin.ngrok-free.app/';
+  // static const String baseUrl = 'http://localhost:8080';
   static const String otpLoginRegistration = '/api/auth/public/otp/login';
   static const String otpAuthentication = '/api/auth/public/otp/authenticate';
   static const String sendSms = '/api/sms/send';
